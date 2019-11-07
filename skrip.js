dfa064952774dcbfeff22e63f94e1baaecac6c00
Reza Ararsy <rezaararsy@gmail.com>
2019-11-07 15:48:31 +0700

    oke

--- skrip.js
+++ skrip.js
@@ -3,6 +3,14 @@ function myFunction() {
     window.location.href = "index.html";
 }
 if (localStorage.getItem("role") == 1) {
-    $("#akune").append('O');
-
+    $("#akune").append('Owner');
+}
+if (localStorage.getItem("role") == 2) {
+    $("#akune").append('Produksi');
+}
+if (localStorage.getItem("role") == 3) {
+    $("#akune").append('Logistik');
+}
+if (localStorage.getItem("role") == 5) {
+    $("#akune").append('Pemasaran');
 }
