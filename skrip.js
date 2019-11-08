e451633f6521128475b122bcfdd8591973b27fb4
Reza Ararsy <rezaararsy@gmail.com>
2019-11-08 11:02:29 +0700

    delete edit

--- skrip.js
+++ skrip.js
@@ -15,6 +15,41 @@ if (localStorage.getItem("role") == 5) {
     $("#akune").append('Pemasaran');
 }
 function hapusProduk(id) {
-    localStorage.clear();
-    window.location.href = "index.html";
+    var result1 = confirm("Apakah anda yakin untuk menghapus data ini ?");
+    if (result1) {
+        event.preventDefault();
+        $.ajax({
+            type: "DELETE",
+            datatype: "json",
+            contentType: "application/json; charset=utf-8",
+            url: "https://hd7w3ziuif.execute-api.us-east-1.amazonaws.com/produk/deleteproduk",
+            data: JSON.stringify({
+                id: id
+            }),
+            success: function (result) {
+                if (result) {
+                    console.log(result);
+                    window.location.href = "stock_produk.html";
+                }
+                else {
+                    alert("Hapus Gagal");
+                }
+            }
+        });
+        return false;
+    }
+}
+function editProduk1(id, namae, jml, harga) {
+    $('#idnya').val(function () {
+        return id;
+    });
+    $('#namaproduk').val(function () {
+        return namae;
+    });
+    $('#jml').val(function () {
+        return jml;
+    });
+    $('#hargae').val(function () {
+        return harga;
+    });
 }
